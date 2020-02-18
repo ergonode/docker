7bfc88df9dfc7bebc9a20288eff7aed6ea9a06e1
Daniel Marynicz <daniel.marynicz@gmail.com>
2020-02-18 16:09:12 +0100

    fix

--- config/postgres/docker-entrypoint-initdb.d/10-create-app-user.sh
+++ config/postgres/docker-entrypoint-initdb.d/10-create-app-user.sh
@@ -2,8 +2,15 @@
 
 set -e
 
+
+password=${APP_USER_PASSWORD}
+if [ -n "${APP_USER_PASSWORD_FILE}" ] ; then
+ password=$(cat "${APP_USER_PASSWORD_FILE}")
+fi
+
 #escape string with double quotes
-password=${APP_USER_PASSWORD/\'/\'\'}
+password=${password/\'/\'\'}
+
 
 psql -v ON_ERROR_STOP=1 \
     --dbname "$POSTGRES_DB" <<-EOSQL
--- config/postgres/postgres-healthcheck.sh
+++ config/postgres/postgres-healthcheck.sh
@@ -2,7 +2,12 @@
 
 host="$(hostname -i || echo '127.0.0.1')"
 
-PGPASSWORD=$POSTGRES_PASSWORD psql \
+password=${POSTGRES_PASSWORD}
+if [ -n "${POSTGRES_PASSWORD_FILE}" ] ; then
+ password=$(cat "${POSTGRES_PASSWORD_FILE}")
+fi
+
+PGPASSWORD=${password} psql \
     --host="$host" \
     --username="$POSTGRES_USER" \
     --dbname="$POSTGRES_DB" \
