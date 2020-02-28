86869339be5f6b27b48c92c9e6a0cec9082a57d8
Daniel Marynicz <daniel.marynicz@gmail.com>
2020-02-28 16:04:08 +0100

    work

--- config/rabbitmq/ergonode-docker-entrypoint.sh
+++ config/rabbitmq/ergonode-docker-entrypoint.sh
@@ -2,51 +2,12 @@
 
 set -e
 
-hostname=$(hostname)
-RABBITMQ_NODENAME=${RABBITMQ_NODENAME:-rabbit}
-
-runUntil() {
-    startTime=$SECONDS
-    until "$@" ; do
-        exitCode=$?
-        sleep 2
-        elapsedTime=$(($SECONDS - $startTime))
-        if (( $elapsedTime > ${START_PERIOD:-300} )); then
-            exit ${exitCode}
-        fi
-    done
-}
-
-echo $hostname
-
 if [[ "$1" = 'rabbitmq-server' ]] ; then
-
-#    if [[ -z "$CLUSTER_WITH" || "$CLUSTER_WITH" == "$hostname" ]]; then
-#        >&2 echo "starting as main server"
-#        exec docker-entrypoint.sh "$@"
-#    else
-
-        function finish {
-            jobs=$(jobs -p)
-            if [[ -n ${jobs} ]] ; then
-                kill $(jobs -p)
-            fi
-        }
-        trap finish EXIT
-
-        export RABBITMQ_LOGS="/var/log/rabbitmq/rabbitmq.log"
-        docker-entrypoint.sh "$@" -detached
-
-        rabbitmqctl stop_app
-        echo "Joining to cluster $CLUSTER_WITH"
-
-        set +e
-        rabbitmqctl join_cluster ${ENABLE_RAM:+--ram} ${RABBITMQ_NODENAME}@${CLUSTER_WITH}
-        set -e
-        rabbitmqctl start_app
-
-        tail -f "${RABBITMQ_LOGS}"
-    #fi
+  hostname -i
+  longHostName=$(dig -x $(hostname -i) +short)
+   apk add bind-tools
+  envsubst < /etc/rabbitmq/rabbitmq.conf.template >  /etc/rabbitmq/rabbitmq.conf
+  exec docker-entrypoint.sh "$@"
 fi
 
 
