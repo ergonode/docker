f535d2dddabd2e360c979590a365eafb93aa7641
Daniel Marynicz <daniel.marynicz@gmail.com>
2020-03-17 14:10:42 +0100

    fix rabbitmq

--- config/rabbitmq/docker-ergonode-entrypoint.sh
+++ config/rabbitmq/docker-ergonode-entrypoint.sh
@@ -3,11 +3,29 @@
 
 set -e
 
-envsubst < /etc/rabbitmq/rabbitmq.conf.template > /etc/rabbitmq/rabbitmq.conf
+function waitUntil() {
+    if  ! "$@" ; then
+     sleep 1
+     waitUntil "$@" ;
+    fi
+}
+
+envsubst < /etc/rabbitmq/rabbitmq.conf.template >> /etc/rabbitmq/rabbitmq.conf
 
 if [[ ! -z "${CONSUL_ACL_TOKEN}" ]] ;
 then
     echo "cluster_formation.consul.acl_token = ${CONSUL_ACL_TOKEN}" >>  /etc/rabbitmq/rabbitmq.conf
 fi
 
-exec docker-entrypoint.sh "$@"
+
+
+>&2 echo "Waiting for consul..."
+waitUntil ping -c 1 "${CONSUL_HOST}"
+
+if [[ ! -z "${WAIT_FOR}" ]] ;
+then
+    >&2 echo "Waiting for ${WAIT_FOR}..."
+    waitUntil ping -c 1 "${WAIT_FOR}"
+fi
+
+exec docker-entrypoint.sh rabbitmq-server
