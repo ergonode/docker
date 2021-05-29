a49c56fea022bf054a2c3772e6ab7c833c4c4d7a
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2021-05-29 12:04:38 +0200

    Cleaned up redundant queues (#73)

--- config/php/docker-entrypoint.sh
+++ config/php/docker-entrypoint.sh
@@ -139,16 +139,12 @@ if [[ "$1" =~ bin/console ]] && [[ "$2" = 'messenger:consume' ]]; then
     createAmqpVhost "${MESSENGER_TRANSPORT_IMPORT_DSN}"
     createAmqpVhost "${MESSENGER_TRANSPORT_CORE_DSN}"
     createAmqpVhost "${MESSENGER_TRANSPORT_EXPORT_DSN}"
-    createAmqpVhost "${MESSENGER_TRANSPORT_SEGMENT_DSN}"
-    createAmqpVhost "${MESSENGER_TRANSPORT_COMPLETENESS_DSN}"
     createAmqpVhost "${MESSENGER_TRANSPORT_NOTIFICATION_DSN}"
     createAmqpVhost "${MESSENGER_TRANSPORT_BATCH_ACTION_DSN}"
 
     bin/console messenger:setup-transports --no-interaction import
     bin/console messenger:setup-transports --no-interaction export
     bin/console messenger:setup-transports --no-interaction core
-    bin/console messenger:setup-transports --no-interaction segment
-    bin/console messenger:setup-transports --no-interaction completeness
     bin/console messenger:setup-transports --no-interaction notification
     bin/console messenger:setup-transports --no-interaction batch_action
 
